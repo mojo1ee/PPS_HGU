a2e9af74fe22da66eb217e8c31fd513f90f78e31
Jandos <joseph3.lee@gmail.com>
2024-01-15 18:57:20 +0900

    4344 up

--- wk1/BJ4344.c
+++ wk1/BJ4344.c
@@ -1,28 +1,39 @@
 // https://www.acmicpc.net/problem/4344
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+int main()
+{
 
     int c, n, score, total, count, len;
-    int arr[1000];
+    int *arr;
     float avg;
     scanf("%d", &c);
-    for(int i = 0; i < c; i++){
+
+    for (int i = 0; i < c; i++)
+    {
         scanf("%d", &n);
+        arr = (int *)malloc((n + 1) * sizeof(int));
         total = 0;
         count = 0;
-        for(int j = 0; j < n; j++){
+
+        for (int j = 0; j < n; j++)
+        {
             scanf("%d", &score);
-            // 
+            arr[j] = score;
+            //
             total += score;
         }
-        avg = (float) total / n;
-        // printf("%f\n", avg);
-        for(int j = 0; j < n; j++){
-            scanf("%d", &score);
-            total += score;
+        avg = (float)total / n;
+
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[j] > avg)
+                count++;
         }
 
+        printf("%.3f%%\n", 100 * (float)count / n);
+        free(arr);
     }
     return 0;
 }
